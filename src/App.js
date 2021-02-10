ae4025b2aa43072b250d7b7d7e0a935454e8caf8
austintip <austin.d.tipograph@gmail.com>
2021-02-09 21:25:39 -0500

    the ships are rendering again

--- src/App.js
+++ src/App.js
@@ -3,29 +3,33 @@ import './App.css';
 
 function App() {
   const [ships, setShips] = useState([])
-  
+  let list
   useEffect(() => {
     fetch('https://swapi.dev/api/starships/')
     .then(response => response.json())
     .then(jsonData => {
       setShips(Object.values(jsonData))
       // console.log(jsonData)
-    });
+    }).catch(err =>{
+      console.log('api err?', err)
+    }
+    )
   }, [])
 
   // console.log(ships[3])
-
-  let list = ships[3].map((ship, i) => {
-    return(
-      <li key={i}>
-        {ship.name}
-      </li>
-    )
-  })
+  if (ships.length > 0) {
+    list = ships[3].map((ship, i) => {
+      return(
+        <li className="ships-list" key={i}>
+          {ship.name}
+        </li>
+      )
+    })
+  }
 
 
   return (
-    <div>
+    <div className="list-holder">
       {list}
     </div>
   )
