f2424110594ca50ae7df83b2d0a7be8b2369cea9
austintip <austin.d.tipograph@gmail.com>
2021-02-09 21:39:37 -0500

    ships are rendering, links didnt work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
 import './App.css';
+import StarshipPage from './StarshipPage'
 
 function App() {
   const [ships, setShips] = useState([])
   let list
+  let shipsInfo
   useEffect(() => {
     fetch('https://swapi.dev/api/starships/')
     .then(response => response.json())
@@ -23,6 +25,17 @@ function App() {
         <li className="ships-list" key={i}>
           {ship.name}
         </li>
+      )
+    })
+  }
+
+  if (ships.length > 0) {
+    shipsInfo = ships[3].map((ship, i) => {
+      return (
+        <StarshipPage key={i}
+        shipName={ship.name}
+        shipInfo={ship.model}
+        />
       )
     })
   }
---
+++ src/StarshipPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StarshipPage = (props) => {
+    return(
+        <div>
+            <h1>starship info</h1>
+        </div>
+    )
+}
+
+export default StarshipPage
